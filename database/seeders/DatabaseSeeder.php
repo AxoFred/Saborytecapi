7c10d81a31a1558fd2b66caeb42c327326d57d7e
Fredy Cano Lopez <canofredy94@gmail.com>
2026-05-17 19:04:30 -0600

    primero usuario

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,30 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Usuario;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Creamos el usuario de prueba de forma manual usando tus campos reales
+        Usuario::create([
+            'nombre'   => 'Fredy',
+            'Apaterno' => 'Cano',
+            'Amaterno' => 'Lopez',
+            'correo'   => 'AxoFred@smartin.tecnm.mx',
+            'password' => Hash::make('Password'), 
+            'telefono' => '2481701307',
+            'estado'   => 'activo',
+            'ID_rol'   => 1,
+            'visible'  => 1
         ]);
     }
 }
